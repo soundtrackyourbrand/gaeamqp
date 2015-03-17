dda6362d413b8e0ca9d107591cb6f3f1e4085c22
frekw <fredrik@soundtrackyourbrand.com>
2015-03-17 13:47:07 +0100

    Simplified GAEAMQP.

--- amqp.go
+++ amqp.go
@@ -1,7 +1,6 @@
 package gaeamqp
 
 import (
-	"fmt"
 	"net"
 	"time"
 
@@ -16,7 +15,7 @@ const (
 	defaultConnectionTimeout = 30 * time.Second
 )
 
-func dial(c appengine.Context, url string) (*amqp.Connection, error) {
+func Dial(c appengine.Context, url string) (*amqp.Connection, error) {
 	return amqp.DialConfig(url, amqp.Config{
 		Heartbeat: defaultHeartbeat,
 		Dial: func(network, addr string) (net.Conn, error) {
@@ -32,55 +31,4 @@ func dial(c appengine.Context, url string) (*amqp.Connection, error) {
 			return conn, nil
 		},
 	})
-}
-
-type context interface {
-	appengine.Context
-
-	GetAMQPAuthority() string
-}
-
-func Publish(c context, message string) (err error) {
-	conn, err := dial(c, fmt.Sprintf("amqp://%s/", c.GetAMQPAuthority()))
-	if err != nil {
-		c.Errorf("Failed to connect to RabbitMQ")
-		return err
-	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		c.Errorf("Failed to open a channel")
-		return err
-	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello",
-		false,
-		true,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		c.Errorf("Failed to declare a queue")
-		return err
-	}
-
-	err = ch.Publish(
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(message),
-		})
-	if err != nil {
-		c.Errorf("Failed to publish a message")
-		return err
-	}
-
-	return nil
 }
