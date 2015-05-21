fd30f39677a7f9e24c39fe501a46f9b08fc7cdc0
Fredrik Enestad <fredrik.enestad@soundtrackyourbrand.com>
2015-05-21 19:12:07 +0200

    Dialer returns a func with same signature as ampq.Dial

--- amqp.go
+++ amqp.go
@@ -15,20 +15,30 @@ const (
 	defaultConnectionTimeout = 30 * time.Second
 )
 
+func appEngineDial(c appengine.Context) func(network, addr string) (net.Conn, error) {
+	return func(network, addr string) (net.Conn, error) {
+		conn, err := socket.DialTimeout(c, network, addr, defaultConnectionTimeout)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := conn.SetReadDeadline(time.Now().Add(defaultConnectionTimeout)); err != nil {
+			return nil, err
+		}
+
+		return conn, nil
+	}
+}
+
+func Dialer(c appengine.Context) func(url string) (*amqp.Connection, error) {
+	return func(url string) (*amqp.Connection, error) {
+		return amqp.DialConfig(url, amqp.Config{
+			Heartbeat: defaultHeartbeat,
+			Dial:      appEngineDial(c),
+		})
+	}
+}
+
 func Dial(c appengine.Context, url string) (*amqp.Connection, error) {
-	return amqp.DialConfig(url, amqp.Config{
-		Heartbeat: defaultHeartbeat,
-		Dial: func(network, addr string) (net.Conn, error) {
-			conn, err := socket.DialTimeout(c, network, addr, defaultConnectionTimeout)
-			if err != nil {
-				return nil, err
-			}
-
-			if err := conn.SetReadDeadline(time.Now().Add(defaultConnectionTimeout)); err != nil {
-				return nil, err
-			}
-
-			return conn, nil
-		},
-	})
+	return Dialer(c)(url)
 }
